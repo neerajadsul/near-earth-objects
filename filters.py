0514412d94a4989c16f5db49b6fc0b9f12cbe082
tantradnya <neeraj.adsul@gmail.com>
2021-03-21 11:17:38 +0000

    cleanup comments and documentation

--- filters.py
+++ filters.py
@@ -106,7 +106,6 @@ def create_filters(date=None, start_date=None, end_date=None,
     :param hazardous: Whether the NEO of a matching `CloseApproach` is potentially hazardous.
     :return: A collection of filters for use with `query`.
     """
-    # TODO: Decide how you will represent your filters.
     filters = []
     if distance_min is not None:
         fdistmin = DistanceFilter(operator.ge, distance_min)
@@ -175,7 +174,6 @@ def limit(iterator, n=None):
     :param n: The maximum number of values to produce.
     :yield: The first (at most) `n` values from the iterator.
     """        
-    # TODO: Produce at most `n` values from the given iterator.
     if n==0 or n is None:
         return iterator    
     
