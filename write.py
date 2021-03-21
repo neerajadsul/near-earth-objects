16e999ad007db364bb2fbc8530b328b954999065
tantradnya <neeraj.adsul@gmail.com>
2021-03-21 17:34:03 +0000

    add implementation of csv writing

--- write.py
+++ write.py
@@ -24,9 +24,20 @@ def write_to_csv(results, filename):
     :param results: An iterable of `CloseApproach` objects.
     :param filename: A Path-like object pointing to where the data should be saved.
     """
-    fieldnames = ('datetime_utc', 'distance_au', 'velocity_km_s', 'designation', 'name', 'diameter_km', 'potentially_hazardous')
     # TODO: Write the results to a CSV file, following the specification in the instructions.
-
+    header = "'datetime_utc', 'distance_au', 'velocity_km_s', 'designation', 'name', 'diameter_km', 'potentially_hazardous'"
+    with open(filename, 'w') as outfile:
+        outfile.write(header + '\n')        
+        for result in results:
+            d = result.serialize()
+            output = ""
+            for v in d.values():
+                output += str(v) + ','
+            d = result.neo.serialize()
+            for v in d.values():
+                output += str(v) + ','
+            outfile.write(output[:-1])
+            outfile.write('\n')
 
 def write_to_json(results, filename):
     """Write an iterable of `CloseApproach` objects to a JSON file.
