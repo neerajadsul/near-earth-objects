f158049e9f2b16617a5c2c828abc3c10ce064075
tantradnya <neeraj.adsul@gmail.com>
2021-03-21 18:34:56 +0000

    final cleanup for submission

--- write.py
+++ write.py
@@ -8,7 +8,6 @@ These functions are invoked by the main module with the output of the `limit`
 function and the filename supplied by the user at the command line. The file's
 extension determines which of these functions is used.
 
-You'll edit this file in Part 4.
 """
 import csv
 import json
@@ -24,7 +23,6 @@ def write_to_csv(results, filename):
     :param results: An iterable of `CloseApproach` objects.
     :param filename: A Path-like object pointing to where the data should be saved.
     """
-    # TODO: Write the results to a CSV file, following the specification in the instructions.
     header = ('datetime_utc', 'distance_au', 'velocity_km_s', 'designation', 'name', 'diameter_km', 'potentially_hazardous')
     with open(filename, 'w') as outfile:
         outfile.write(','.join(header))
@@ -51,17 +49,13 @@ def write_to_json(results, filename):
     :param results: An iterable of `CloseApproach` objects.
     :param filename: A Path-like object pointing to where the data should be saved.
     """
-    # TODO: Write the results to a JSON file, following the specification in the instructions.
     output = []
     for result in results:
         d1 = result.serialize()
-        # print(d1)
         d2 ={}
         d2['neo'] = result.neo.serialize()
-        # print(d2)
         d1.update(d2)
         output.append(d1)
-        # break
     print(output)
     with open(filename, 'w') as outfile:
         json.dump(output, outfile, indent=2)
